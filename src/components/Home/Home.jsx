f70ff8c0031b605cc2a6daf0ceaddb1cb4ec1f30
duyduc2003 <dangduyducdangduyduc@gmail.com>
2023-06-01 13:30:31 +0700

    init branch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,24 @@
-import React, {useEffect} from 'react'
-import './Home.css'
-import video from '../../Assets/video.mp4'
-import {GrLocation} from 'react-icons/gr'
-import {HiFilter} from 'react-icons/hi'
-import {FiFacebook} from 'react-icons/fi'
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {FaTripadvisor} from 'react-icons/fa'
-import {TbApps} from 'react-icons/tb'
-import {BsListTask} from 'react-icons/bs'
+import React, { useEffect } from "react";
+import "./Home.css";
+import video from "../../Assets/video.mp4";
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { TbApps } from "react-icons/tb";
+import { BsListTask } from "react-icons/bs";
 
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-  useEffect(()=>{
-    Aos.init({duration: 2000})
-  }, [])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
 
   return (
-    <section className='home'>
+    <section className="home">
       <div className="overlay"></div>
       <video src={video} muted autoPlay loop type="video/mp4"></video>
 
@@ -27,7 +27,7 @@ const Home = () => {
           <span data-aos="fade-up" className="smallText">
             Our Packages
           </span>
-          <h1 data-aos="fade-up" className='homeTitle'>
+          <h1 data-aos="fade-up" className="homeTitle">
             Search your Holiday
           </h1>
         </div>
@@ -36,22 +36,22 @@ const Home = () => {
           <div className="destinationInput">
             <label htmlFor="city">Search your destination.</label>
             <div className="input flex">
-              <input type="text" placeholder='Enter name here...'/>
-              <GrLocation className="icon"/>
+              <input type="text" placeholder="Enter name here..." />
+              <GrLocation className="icon" />
             </div>
           </div>
 
           <div className="dateInput">
             <label htmlFor="date">Select your date</label>
             <div className="input flex">
-              <input type="date"/>
+              <input type="date" />
             </div>
           </div>
           {/* oke */}
           <div className="priceInput">
             <div className="label_total flex">
               <label htmlFor="price">Max price:</label>
-              <h3 className='total'>$5000</h3>
+              <h3 className="total">$5000</h3>
             </div>
             <div className="input flex">
               <input type="range" max="5000" min="1000" />
@@ -59,27 +59,26 @@ const Home = () => {
           </div>
 
           <div className="searchOptions flex">
-            <HiFilter className="icon"/>
+            <HiFilter className="icon" />
             <span>MORE FILTERS</span>
           </div>
         </div>
 
         <div data-aos="fade-up" className="homeFooterIcons flex">
           <div className="rightIcons">
-            <FiFacebook className="icon"/>
-            <AiOutlineInstagram className="icon"/>
-            <FaTripadvisor className="icon"/>
+            <FiFacebook className="icon" />
+            <AiOutlineInstagram className="icon" />
+            <FaTripadvisor className="icon" />
           </div>
 
           <div className="leftIcons">
-            <BsListTask className="icon"/>
-            <TbApps className="icon"/>
-
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
