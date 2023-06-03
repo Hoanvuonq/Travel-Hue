d5f2e3fc62e38ffd3fcbcfab688e3e7bcd8c6ff7
Hoanvuonq <hoanvuonq2002@gmail.com>
2023-06-03 17:04:50 +0700

    Popup Detail Item/blog

---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { routes } from '../../utils/routes';
+import MainLayout from '../../layouts/MainLayout';
+import './Blog.css'
+
+
+const Blog = ({ imgSrc, description, closePopup }) => {
+    return (
+      <div className="popup">
+        <div className="popup-content">
+          <div className="popup-container">
+            <img src={imgSrc} alt="Popup Image" />
+            <p>{description}</p>
+            <button onClick={closePopup}>X</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+Blog.options = {
+  layout: MainLayout,
+  route: routes.BLOG
+};
+
+export default Blog;
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,4 +1,5 @@
 export  const routes = {
   HOME: '/',
-  FORM: '/form'
+  FORM: '/form',
+  BLOG: '/blog',
 }
